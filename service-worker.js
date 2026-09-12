413ab4adce864a51a8d1e1a1c5558e6a527e571f
furkandur-lab <muhammedfurkandur@gmail.com>
2026-09-13 02:23:40 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+var CACHE_NAME = 'guzergah-cache-v1';
+var CACHE_URLS = ['./', './index.html'];
+
+self.addEventListener('install', function(event){
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache){
+      return cache.addAll(CACHE_URLS);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event){
+  event.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.filter(function(k){ return k !== CACHE_NAME; }).map(function(k){ return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event){
+  if(event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request).then(function(resp){
+      var copy = resp.clone();
+      caches.open(CACHE_NAME).then(function(cache){ cache.put(event.request, copy); });
+      return resp;
+    }).catch(function(){
+      return caches.match(event.request).then(function(cached){
+        return cached || caches.match('./index.html');
+      });
+    })
+  );
+});
